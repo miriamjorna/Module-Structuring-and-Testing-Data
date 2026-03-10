75ed5d6fb7a36c1e5cf44ecaaadf10d11c427c5c
miriamjorna <hoimir@gmail.com>
2026-03-10 19:48:39 +0000

    prepping for coursework PR by moving away the practice files

--- Sprint-3/ordinal-testing-example/get-ordinal-number.test.js
+++
@@ -1,9 +0,0 @@
-function getOrdinalNumber() {
-  return "1st";
-}
-
-test("works for any number ending in 1", () => {
-  expect(getOrdinalNumber(1)).toEqual("1st");
-  expect(getOrdinalNumber(11)).toEqual("11th");
-  expect(getOrdinalNumber(21)).toEqual("21st");
-});
