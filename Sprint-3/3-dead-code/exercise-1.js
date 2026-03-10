911259d0f3191e045b601288651d6389c34a2d6d
miriamjorna <hoimir@gmail.com>
2026-03-10 20:04:55 +0000

    Committing Sprint-3/3-dead-code/exercise-1.js

--- Sprint-3/3-dead-code/exercise-1.js
+++ Sprint-3/3-dead-code/exercise-1.js
@@ -1,16 +1,12 @@
 // Find the instances of unreachable and redundant code - remove them!
 // The sayHello function should continue to work for any reasonable input it's given.
 
-let testName = "Jerry";
 const greeting = "hello";
+const testName = "Aman";
 
-function sayHello(greeting, name) {
-  const greetingStr = greeting + ", " + name + "!";
+const sayHello = (greeting, name) => {
   return `${greeting}, ${name}!`;
-  console.log(greetingStr);
-}
-
-testName = "Aman";
+};
 
 const greetingMessage = sayHello(greeting, testName);
 
